6b18e8619891011b4f22417abb12c8fa1b016016
saket <saket@saketgupta.com>
2026-01-20 23:56:37 +0530

    working on the type script learning

--- src/app/(dashboard)/_component/DashboardSidebar.tsx
+++ src/app/(dashboard)/_component/DashboardSidebar.tsx
@@ -1,9 +1,11 @@
 import { Sidebar } from "@/components/ui/sidebar";
 
 
-export default function DashboardSidebar(){
-    return(
-        <Sidebar></Sidebar>
+export default function DashboardSidebar() {
+    return (
+        <Sidebar>
+
+        </Sidebar>
     )
 }
 // 2:36
