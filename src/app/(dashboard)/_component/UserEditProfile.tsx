7bfdadf9223b083c7de4ccbdb1a1e828a71d2ccb
saket <guptasaket898@gmail.com>
2026-02-04 23:59:46 +0530

    feat(UserEditProfile): implement form structure with input fields for user details

--- src/app/(dashboard)/_component/UserEditProfile.tsx
+++ src/app/(dashboard)/_component/UserEditProfile.tsx
@@ -1,3 +1,6 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 interface UserEditProfile {
     firstName: string | undefined;
     lastName: string | undefined;
@@ -6,6 +9,11 @@ interface UserEditProfile {
 }
 export default async function UserEditProfilePAge({ firstName, lastName, email, currency }: UserEditProfile) {
     return (
-        <div></div>
+        <form>
+            <div>
+                <Label>First Name</Label>
+                <Input placeholder="John" type="text" />
+            </div>
+        </form>
     )
 }
